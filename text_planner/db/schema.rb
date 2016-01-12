45b8f5c5081ea2057dd823e8f1c54308f8412c64
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-01-11 21:34:22 -0800

    generated user event models

--- text_planner/db/schema.rb
+++ text_planner/db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111014922) do
+ActiveRecord::Schema.define(version: 20160112022411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.string   "location"
+    t.date     "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_events", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_events", ["event_id"], name: "index_user_events_on_event_id", using: :btree
+  add_index "user_events", ["user_id"], name: "index_user_events_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -23,4 +41,6 @@ ActiveRecord::Schema.define(version: 20160111014922) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_events", "events"
+  add_foreign_key "user_events", "users"
 end
