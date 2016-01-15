c3d92af8396f9e6ac304c103863464563a68b54e
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-01-14 18:31:13 -0800

    added bookmarks controller

--- text_planner/app/models/user.rb
+++ text_planner/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :friends, :through => :friendships
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
