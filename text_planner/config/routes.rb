14753322656bbafa5903517c1f26f4c4499e75c2
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-01-14 22:42:04 -0800

    generated user bookmarks friendships comments routes but some may not be necessary

--- text_planner/config/routes.rb
+++ text_planner/config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 
-  # resources 
+  resources :users do
+    resources :bookmarks, shallow: true
+    resources :friendships, shallow: true
+    resources :comments, shallow: true
+  end
+
+  resources :bookmarks do
+    resources :comments, shallow: true
+  end
+
 end
