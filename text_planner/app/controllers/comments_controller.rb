6bf626dd62cc7a68e1a066779a0a5fd60003d04a
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-01-15 12:01:40 -0800

    added methods to comments controller

--- text_planner/app/controllers/comments_controller.rb
+++ text_planner/app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- text_planner/app/controllers/registrations_controller.rb
+++ text_planner/app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :phone_nubmer, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :phone_number, :email, :password, :password_confirmation, :current_password)
   end
   
 end
