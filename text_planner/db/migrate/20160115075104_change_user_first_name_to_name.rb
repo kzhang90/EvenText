81128d45d88036c0427fffb9fb752cabb937c689
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-01-15 16:19:55 -0800

    adding home page spa

---
+++ text_planner/db/migrate/20160115075104_change_user_first_name_to_name.rb
@@ -0,0 +1,5 @@
+class ChangeUsersFirstNameToName < ActiveRecord::Migration
+  def change
+    rename_column :users, :first_name, :name
+  end
+end
--- text_planner/lib/apis/eventbrite.rb
+++ text_planner/lib/apis/eventbrite.rb
@@ -1,7 +1,19 @@
 require 'HTTParty'
   
 class EventBriteApi
-  def eventbrite
-    HTTParty.get()
+  base_uri 'eventbriteapi.com'
+  # start_date.range_start: "2010-01-31T13:00:00Z"
+  # start_date.range_end: "2015-01-31T13:00:00Z"
+  # q: event keyword
+  # venue.city
+  # hard-code: sort_by, best
+  # header authorization "Bearer H3MJZUEJ6CMP2XNVST3C"
+
+  def initialize(keyword, city, start_date, end_date)
+    @options = { query: {q: keyword, venue.city: city, start_date.range_start: start_date, start_date.range_end: end_date}}
+  end
+
+  def events
+    self.class.get("/v3/events", @options)
   end
 end
