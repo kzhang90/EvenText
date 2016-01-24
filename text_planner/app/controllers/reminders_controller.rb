30260e10c6a49f07559546709a67231267f0191f
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-01-23 20:35:58 -0800

    add reminder views

--- text_planner/app/controllers/reminders_controller.rb
+++ text_planner/app/controllers/reminders_controller.rb
@@ -11,6 +11,18 @@ class RemindersController < ApplicationController
     end
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
   def destroy
     @reminder.destroy
     respond_to do |format|
