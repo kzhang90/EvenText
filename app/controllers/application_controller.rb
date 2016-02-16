3f3d6c1f97a304057e3d78f0c94c9058b83051d7
Kenny Zhang <Kenny.y.zhang@gmail.com>
2016-02-16 12:30:02 -0800

    format user show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,7 +55,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:avatar) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :avatar, :password, :current_password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password)}
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :avatar, :password, :password_confirmation) }
   end
 end
